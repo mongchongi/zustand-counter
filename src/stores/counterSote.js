3a296ea5085d227bf2dec84d067a50d5b5fdf3bb
mongchongi <yeonguk0517@gmail.com>
2025-04-12 02:35:12 +0900

    reset 기능 추가

--- src/stores/counterSote.js
+++ src/stores/counterSote.js
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 
 const counterStore = create((set) => ({
   count: 0,
+  reset: () => set(() => ({ count: 0 })),
   increase: (number) => set((state) => ({ count: state.count + number })),
   decrease: (number) => set((state) => ({ count: state.count - number })),
 }));
